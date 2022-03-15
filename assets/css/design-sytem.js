cb29b90458080be528445e26ec4d83043719b437
Stephan Taylor <stephantaylor21@hotmail.com>
2022-03-15 15:58:05 -0600

    added design-system.js

---
+++ assets/css/design-sytem.js
@@ -0,0 +1,6 @@
+const theme = { colors: { 'button-color': '#e7e5e5' },
+  fontSize: { base: '1.625rem', lg: '2.25rem', xl: '4rem' },
+  fontFamily: 
+   { 'font-awesome-5-brands': 'Font Awesome 5 Brands',
+     bitter: 'Bitter' },
+  borderRadius: { none: '0', xs: '0.4375rem', sm: '1.0625rem' } }; export default theme;
